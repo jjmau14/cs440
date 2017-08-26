72aa9c81f08b56f7237e53de8a0d56e7ba0c7c53
jjmau14 <jjmau14@gmail.com>
2017-08-26 16:46:03 -0600

    dfs working

--- hw1.py
+++ hw1.py
@@ -1,49 +1,27 @@
 from copy import copy
 
-def breadthFirstSearch(startState, goalState, successorsf):
-	bfs = []
-	stack = list(startState)
-	
-	while stack:
-		print(stack)
-		node = stack.pop(0)
-		if node == goalState:
-			bfs.append(goalState)
-			return bfs
-		if not node in bfs:
-			bfs.append(node)
-			if successorsf(node):
-				for child in successorsf(node):
-					stack.append(child)
-	return bfs
+def breadthFirstSearch(start, end, successorsf, visited=[], queue=[], path=[]):
+	pass
 		
 	
-def depthFirstSearch(start, end, successorsf, visited=[], parentTable={}):
-	# Initialize stack with start
-	stack = list(start)
+def depthFirstSearch(start, end, successorsf, visited=[], path=[]):
+	path .append(start)
 	
-	# While something is still in the stack
-	while stack:
-		print(stack)
-		# Pop and visit top of stack
-		node = stack.pop()
-		visited.append(node)
-		
-		if successorsf(node):
-			for child in successorsf(node):
+	if start is end:
+		return path
+	
+	else:
+		if successorsf(start):
+			for child in successorsf(start):
 				if child not in visited:
-					stack.append(child)
-					parentTable[child] = node
+					visited.append(child)
+					current_path = depthFirstSearch(child, end, successorsf, visited, path)
+					if current_path:
+						return current_path
+					else:
+						path.pop()
+	return None
 
-	return getPath(start, end, parentTable)
-
-def getPath(start, end, parents, path=[]):
-	node = end
-	while node != start:
-		path.insert(0, node)
-		node = parents[node]
-	path.insert(0, start)
-	return path
 	
 successors = {'a': ['b', 'c', 'd'],
  'b': ['e', 'f', 'g'],
@@ -57,4 +35,4 @@ def successorsf(state):
 	return copy(successors.get(state))	
 
 if __name__ == '__main__':
-	print(depthFirstSearch('a', 'z', successorsf))
+	print(depthFirstSearch('a', 'm', successorsf))
