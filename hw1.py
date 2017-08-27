629edff4e8299560c2006267de10ae68bbffc19f
jjmau14 <jjmau14@gmail.com>
2017-08-26 18:16:12 -0600

    Full Functionality DFS/BFS

--- hw1.py
+++ hw1.py
@@ -1,8 +1,27 @@
 from copy import copy
 
-def breadthFirstSearch(start, end, successorsf, visited=[], queue=[], path=[]):
-	pass
+def breadthFirstSearch(start, end, successorsf, visited=[], queue=[], parentTree={}):
+	queue.append(start)
+	
+	while queue:
+		node = queue.pop()
+		visited.append(node)
 		
+		if node is end:
+			return getTree(parentTree, start, end)
+		
+		if successorsf(node):
+			for child in successorsf(node):
+				queue.insert(0, child)
+				parentTree[child] = node
+				
+def getTree(parents, start, end, path=[]):
+	curr = end
+	while curr != start:
+		path.insert(0, curr)
+		curr = parents[curr]
+	path.insert(0,curr)
+	return path
 	
 def depthFirstSearch(start, end, successorsf, visited=[], path=[]):
 	path.append(start)
@@ -35,4 +54,4 @@ def successorsf(state):
 	return copy(successors.get(state))	
 
 if __name__ == '__main__':
-	print(depthFirstSearch('a', 'm', successorsf))
+	print(breadthFirstSearch('a', 'z', successorsf))
